aed38a479588469c5eb2a563bd351794c1c2bee5
Zack Fertig <61804342+zfert514@users.noreply.github.com>
2025-02-04 14:56:04 -0500

    Initial Commit

---
+++ eartrainer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eartrainer
+//
+//  Created by Zachary Fertig on 2/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eartrainerTests/eartrainerTests.swift
@@ -0,0 +1,17 @@
+//
+//  eartrainerTests.swift
+//  eartrainerTests
+//
+//  Created by Zachary Fertig on 2/4/25.
+//
+
+import Testing
+@testable import eartrainer
+
+struct eartrainerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
